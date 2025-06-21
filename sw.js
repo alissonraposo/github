e6d9129903feec831441faafe7881316d3c33559
alissonraposo <alissonraposo@hotmail.com>
2025-06-21 07:46:26 -0300

    ...

--- sw.js
+++ sw.js
@@ -1,115 +1,6 @@
-var CACHE_NAME = 'static-v013a33';
-let dbName = "dbTeste";
-let tbUsuario = "usuario";
-
-var ativo = false;
-setInterval(async () => {
-
-  try {
-    if(self.registration.active.state == 'activated'){
-      // self.registration.showNotification("actived", {body: "self.registration.active.state == 'activated'"});
-      let usuarios = await getAllToArray(tbUsuario);
-      let usuario = usuarios[0];
-      await getUsuarioPhp(usuario).then((res) => {
-        self.registration.showNotification("resposta php", {body: res[0].nome});
-        //console.log(self.registration);
-      }).catch((erro) => {
-        self.registration.showNotification("erro resposta php", {body: erro.message});
-      });     
-      self.registration.showNotification("actived2", {body: "depois do acesso web"});
-    }
-    
-  } catch (erro) {
-    // ativo = false;
-    self.registration.showNotification("erro", {body: erro.message});
-    console.log("NAO ESTA ATIVO",erro);
-  }
-  
-}, 30000);
-
-function getAllToArray(tabela){
-  return new Promise((resolve, reject) => {
-      abrirBancoTabelaIdb(tabela, (objectStore) => {
-          let getRegistros = objectStore.getAll();
-          getRegistros.onsuccess = (event) => {
-              let registros = getRegistros.result;
-              resolve(registros);
-          };
-      });                
-  })
-}
-
-function abrirBancoTabelaIdb(tabela, funcao){
-  let request = self.indexedDB.open(dbName,1);
-  request.onerror = function (event) { console.log(`request.onerror -> ${tabela}`); };    
-  request.onsuccess = function (event) {
-      let db = event.target.result;
-      let transaction = db.transaction([tabela], "readwrite");
-      transaction.oncomplete = function (event) {            
-          // console.log(`transaction.oncomplete -> ${tabela}`);   
-      };
-      transaction.onerror = function (event){
-          log(`transaction.onerror -> ${tabela}`);
-      };        
-      let objectStore = transaction.objectStore(tabela);
-      funcao(objectStore);
-  };
-}
-
-async function getUsuarioPhp (usuario){    
-  let url = "https://saaepenedo.criarsite.online/appos/dao/getUsuarioAtualizar.php"; 
-  const conexao = await fetch(url,{
-      method: "POST",
-      headers: {
-          "Content-type": "application/json"
-      },
-      body: JSON.stringify({
-          usuario: {id: usuario.id, status: "A"},
-          "id": usuario.id,
-      })
-  });
-  resposta = await conexao.json();
-  // console.log("id ", id);
-  // console.log("resposta ", resposta);
-  return resposta;
-}
-
-  self.addEventListener("notificationclick", (event) => {
-    console.log("On notification click: ", event.notification.tag);
-    event.notification.close();
-  
-    // This looks to see if the current is already open and
-    // focuses if it is
-    // event.waitUntil(
-    //   clients
-    //     .matchAll({
-    //       type: "window",
-    //     })
-    //     .then((clientList) => {
-    //       for (const client of clientList) {
-    //         if (client.url === "/" && "focus" in client) return client.focus();
-    //       }
-    //       if (clients.openWindow) return clients.openWindow("/");
-    //     }),
-    // );
-  });
-
-self.addEventListener("periodicsync", (event) => {
-  let minhaTag = "minha-tag";
-  let minhaTag2 = "minha-tag2";
-  if(event.tag === minhaTag){
-    console.log("if true");
-    self.registration.showNotification("notificacao do sync", {body: "periodicsync test"});
-  }else{
-    console.log("if false", event.tag);
-  }
-  if(event.tag === minhaTag2){
-    console.log("if true 2");
-    self.registration.showNotification("notificacao do sync2", {body: "periodicsync test2"});
-  }else{
-    console.log("if false 2", event.tag);
-  }
-});
+var CACHE_NAME = 'static-v014';
+// let dbName = "dbTeste";
+// let tbUsuario = "usuario";
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
@@ -125,26 +16,6 @@ self.addEventListener('install', function (event) {
     })
   )
 });
-
-//setInterval(async () => {
-  //const result = await checkOnlineStatus();
-  // const statusDisplay = document.getElementById("statusOnline");
-  // statusDisplay.innerHTML = result ? "Online" : "OFFline";
-  // statusDisplay.innerHTML = result ? "" : "Trabalhando offline";
-  //console.log("interval check", result);
-  //var notification = new Notification("Minha notificacao. status online="+result);
-//}, 30000);
-
-//const checkOnlineStatus = async () => {
-  //try {
-      //let url = urlServidor + "/appos/dao/checkIfOnline.php";
-      //const online = await fetch(url);
-      //return online.status >= 200 && online.status < 300; // true ou false
-  //} catch (err) {
-      //return false; // definitivamente off-line
-  //}
-//};
-
 
 //fetch
 function isCacheable(request) {
@@ -198,6 +69,136 @@ self.addEventListener("activate", (e) => {
 let log = (texto) => {
   console.log(texto);
 }
+
+// var ativo = false;
+// setInterval(async () => {
+
+//   try {
+//     if(self.registration.active.state == 'activated'){
+//       // self.registration.showNotification("actived", {body: "self.registration.active.state == 'activated'"});
+//       let usuarios = await getAllToArray(tbUsuario);
+//       let usuario = usuarios[0];
+//       await getUsuarioPhp(usuario).then((res) => {
+//         self.registration.showNotification("resposta php", {body: res[0].nome});
+//         //console.log(self.registration);
+//       }).catch((erro) => {
+//         self.registration.showNotification("erro resposta php", {body: erro.message});
+//       });     
+//       self.registration.showNotification("actived2", {body: "depois do acesso web"});
+//     }
+    
+//   } catch (erro) {
+//     // ativo = false;
+//     self.registration.showNotification("erro", {body: erro.message});
+//     console.log("NAO ESTA ATIVO",erro);
+//   }
+  
+// }, 30000);
+
+// function getAllToArray(tabela){
+//   return new Promise((resolve, reject) => {
+//       abrirBancoTabelaIdb(tabela, (objectStore) => {
+//           let getRegistros = objectStore.getAll();
+//           getRegistros.onsuccess = (event) => {
+//               let registros = getRegistros.result;
+//               resolve(registros);
+//           };
+//       });                
+//   })
+// }
+
+// function abrirBancoTabelaIdb(tabela, funcao){
+//   let request = self.indexedDB.open(dbName,1);
+//   request.onerror = function (event) { console.log(`request.onerror -> ${tabela}`); };    
+//   request.onsuccess = function (event) {
+//       let db = event.target.result;
+//       let transaction = db.transaction([tabela], "readwrite");
+//       transaction.oncomplete = function (event) {            
+//           // console.log(`transaction.oncomplete -> ${tabela}`);   
+//       };
+//       transaction.onerror = function (event){
+//           log(`transaction.onerror -> ${tabela}`);
+//       };        
+//       let objectStore = transaction.objectStore(tabela);
+//       funcao(objectStore);
+//   };
+// }
+
+// async function getUsuarioPhp (usuario){    
+//   let url = "https://saaepenedo.criarsite.online/appos/dao/getUsuarioAtualizar.php"; 
+//   const conexao = await fetch(url,{
+//       method: "POST",
+//       headers: {
+//           "Content-type": "application/json"
+//       },
+//       body: JSON.stringify({
+//           usuario: {id: usuario.id, status: "A"},
+//           "id": usuario.id,
+//       })
+//   });
+//   resposta = await conexao.json();
+//   // console.log("id ", id);
+//   // console.log("resposta ", resposta);
+//   return resposta;
+// }
+
+  // self.addEventListener("notificationclick", (event) => {
+  //   console.log("On notification click: ", event.notification.tag);
+  //   event.notification.close();
+  
+  //   // This looks to see if the current is already open and
+  //   // focuses if it is
+  //   // event.waitUntil(
+  //   //   clients
+  //   //     .matchAll({
+  //   //       type: "window",
+  //   //     })
+  //   //     .then((clientList) => {
+  //   //       for (const client of clientList) {
+  //   //         if (client.url === "/" && "focus" in client) return client.focus();
+  //   //       }
+  //   //       if (clients.openWindow) return clients.openWindow("/");
+  //   //     }),
+  //   // );
+  // });
+
+// self.addEventListener("periodicsync", (event) => {
+//   let minhaTag = "minha-tag";
+//   let minhaTag2 = "minha-tag2";
+//   if(event.tag === minhaTag){
+//     console.log("if true");
+//     self.registration.showNotification("notificacao do sync", {body: "periodicsync test"});
+//   }else{
+//     console.log("if false", event.tag);
+//   }
+//   if(event.tag === minhaTag2){
+//     console.log("if true 2");
+//     self.registration.showNotification("notificacao do sync2", {body: "periodicsync test2"});
+//   }else{
+//     console.log("if false 2", event.tag);
+//   }
+// });
+
+//setInterval(async () => {
+  //const result = await checkOnlineStatus();
+  // const statusDisplay = document.getElementById("statusOnline");
+  // statusDisplay.innerHTML = result ? "Online" : "OFFline";
+  // statusDisplay.innerHTML = result ? "" : "Trabalhando offline";
+  //console.log("interval check", result);
+  //var notification = new Notification("Minha notificacao. status online="+result);
+//}, 30000);
+
+//const checkOnlineStatus = async () => {
+  //try {
+      //let url = urlServidor + "/appos/dao/checkIfOnline.php";
+      //const online = await fetch(url);
+      //return online.status >= 200 && online.status < 300; // true ou false
+  //} catch (err) {
+      //return false; // definitivamente off-line
+  //}
+//};
+
+
 
 
 // self.addEventListener("fetch", (event) => {
